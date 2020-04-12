26afd87ba903157d47576abac1a87bfcccdda04b
Kirill Kuyan <progingisfun@gmail.com>
2020-04-12 15:07:38 +0300

    Added source configuration
    Signed-off-by: Kirill Kuyan <progingisfun@gmail.com>

---
+++ restservice/src/main/java/aoss/assignment/restservice/config/SpringJdbcConfig.java
@@ -0,0 +1,51 @@
+package aoss.assignment.restservice.config;
+
+/* Created by Kuyan Kirill
+   Email: progingisfun@gmail.com
+   Date: 12.04.2020 */
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("aoss.assignment.restservice")
+public class SpringJdbcConfig {
+
+    @Bean(name = "inventorySource")
+    public DataSource mysqlInventoryDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/inventory?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("adminadmin");
+
+        return dataSource;
+    }
+
+    @Bean(name = "orderInfoSource")
+    public DataSource mysqlOrderInfoDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/orderinfo?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("adminadmin");
+
+        return dataSource;
+    }
+
+    @Bean(name = "inventoryJdbcTemplate")
+    public JdbcTemplate inventoryJdbcTemplate(@Qualifier("inventorySource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean(name = "orderInfoJdbcTemplate")
+    public JdbcTemplate orderInfoJdbcTemplate(@Qualifier("orderInfoSource") DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+}
