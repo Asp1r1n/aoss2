8e51c7109d505ef02c7e3f125db958579b09d45b
Anjasmoro Adi Nugroho <anjasmoro.adi29@gmail.com>
2020-04-13 10:35:08 +0300

    Finish get list function

--- a2/src/main/java/aoss/assignment/a2/merged/views/InventoryManagerFrame.java
+++ a2/src/main/java/aoss/assignment/a2/merged/views/InventoryManagerFrame.java
@@ -1,9 +1,13 @@
 package aoss.assignment.a2.merged.views;
 
 
+import aoss.assignment.a2.merged.controllers.inventory.*;
+import aoss.assignment.a2.merged.models.Item;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class InventoryManagerFrame extends JFrame {
     private JPanel mainPanel;
@@ -14,16 +18,24 @@ public class InventoryManagerFrame extends JFrame {
     private JButton decrementButton;
     private JRadioButton rdTrees;
     private JRadioButton rdSeeds;
-    private JRadioButton rdShu;
-    private JRadioButton referenceMaterialsRadioButton;
-    private JRadioButton cultureBoxesRadioButton;
-    private JRadioButton geonomicsRadioButton;
-    private JRadioButton processingRadioButton;
+    private JRadioButton rdShurbs;
+    private JRadioButton rdReferencematerials;
+    private JRadioButton rdCultureboxes;
+    private JRadioButton rdGenomics;
+    private JRadioButton rdProcessing;
     private JTextField textField2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField1;
     private JButton addItemButton;
+
+    private TreesController treesController;
+    private ShrubsController shrubsController;
+    private SeedsController seedsController;
+    private CultureboxesController cultureboxesController;
+    private GenomicsController genomicsController;
+    private ReferencematerialsController referencematerialsController;
+    private ProcessingController processingController;
 
     public InventoryManagerFrame() {
         add(mainPanel);
@@ -33,6 +45,14 @@ public class InventoryManagerFrame extends JFrame {
 
         initComponent();
         initMenu();
+        initController();
+
+        listInventoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                getList();
+            }
+        });
     }
 
     private void initMenu() {
@@ -73,6 +93,68 @@ public class InventoryManagerFrame extends JFrame {
 
     private void initComponent() {
         //TODO: Handle Component
+    }
+
+    private void initController() {
+        treesController = new TreesController();
+        shrubsController = new ShrubsController();
+        seedsController = new SeedsController();
+        cultureboxesController = new CultureboxesController();
+        genomicsController = new GenomicsController();
+        processingController = new ProcessingController();
+        referencematerialsController = new ReferencematerialsController();
+    }
+
+    private void getList() {
+        String selectedCategory = null;
+        String result = "";
+
+        ArrayList<Item> items = new ArrayList();
+        if (rdTrees.isSelected()) {
+            items = treesController.all();
+            selectedCategory = "TREE";
+        }
+
+        if (rdShurbs.isSelected()) {
+            items = shrubsController.all();
+            selectedCategory = "SHURB";
+        }
+
+        if (rdSeeds.isSelected()) {
+            items = seedsController.all();
+            selectedCategory = "SEED";
+        }
+
+        if (rdCultureboxes.isSelected()) {
+            items = cultureboxesController.all();
+            selectedCategory = "CULTUREBOX";
+        }
+
+        if (rdGenomics.isSelected()) {
+            items = genomicsController.all();
+            selectedCategory = "GENOMIC";
+        }
+
+        if (rdProcessing.isSelected()) {
+            items = processingController.all();
+            selectedCategory = "PROCESSING";
+        }
+
+        if (rdReferencematerials.isSelected()) {
+            items = referencematerialsController.all();
+            selectedCategory = "REFERENCEMATERIAL";
+        }
+
+        if (selectedCategory != null) {
+            for (Item item : items) {
+                result += item.printItem(selectedCategory);
+                result += "\n";
+            }
+        } else {
+            result = "Must select Category button";
+        }
+
+        taResult.setText(result);
     }
 
     public static void main(String[] args) {
